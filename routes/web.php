6ab363bc51881b2c5203c62378dd9c5681f1299d
selviaa01 <selvianirmala01@gmail.com>
2023-03-28 17:20:05 +0700

    pertemuan 4

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,18 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', function () {
-    return view('home', ['title' => 'Home']);
-})->name('home');
 
 Route::get('register', [UserController::class, 'register'])->name('register');
 Route::post('register', [UserController::class, 'register_action'])->name('register.action');
 Route::get('login', [UserController::class, 'login'])->name('login');
 Route::post('login', [UserController::class, 'login_action'])->name('login.action');
-Route::get('password', [UserController::class, 'password'])->name('password');
-Route::post('password', [UserController::class, 'password_action'])->name('password.action');
-Route::get('logout', [UserController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(
+    function() {
+        Route::get('/', function () {
+            return view('home', ['title' => 'Beranda']);
+        })->name('home');
+        Route::get('password', [UserController::class, 'password'])->name('password');
+        Route::post('password', [UserController::class, 'password_action'])->name('password.action');
+        Route::get('logout', [UserController::class, 'logout'])->name('logout');        
+    });
