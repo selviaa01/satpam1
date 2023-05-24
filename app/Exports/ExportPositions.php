ecfa018fc1ec1f5283495480c577944d00c20423
selviaa01 <selvianirmala01@gmail.com>
2023-05-24 16:32:14 +0700

    excel

--- app/Exports/ExportPositions.php
+++ app/Exports/ExportPositions.php
@@ -1,46 +1,17 @@
 <?php
 
-namespace App\Models;
+namespace App\Exports;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use App\Models\Positions;
+use Maatwebsite\Excel\Concerns\FromCollection;
 
-class User extends Authenticatable
+class ExportPositions implements FromCollection
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name', 
-        'email', 
-        'password',
-        'position',
-        'departement'
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        return Positions::all();
+    }
 }
